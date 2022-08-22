e8fb33ea1a916025f2b6a666d602cfa1960aa60b
Tahmina <tahminakhatun5447@gmail.com>
2022-08-22 17:40:17 +0600

    add list

---
+++ js/script.js
@@ -0,0 +1,64 @@
+
+function clickButton(nameId,btnId){
+    const playerNameId=document.getElementById(nameId);
+    const playerName=playerNameId.innerText;
+
+    const node = document.createElement("li");
+    const textnode = document.createTextNode(playerName);
+    node.appendChild(textnode);
+    document.getElementById("ol").appendChild(node);
+
+    let lists=document.querySelectorAll('li');
+    let listQuantity=lists.length;
+    
+    
+    // if(listQuantity>4){
+    //     alert('You can not select more than 5 players.')
+    // }
+    document.getElementById(btnId).disabled = true;
+    document.getElementById(btnId).style.backgroundColor='gray';
+}
+
+
+function input(inputField){
+    const Input=document.getElementById(inputField);
+    const InputString=Input.value;
+    const inputValue=parseInt(InputString);
+    return inputValue;
+}
+
+
+
+document.getElementById('calculate-btn').addEventListener('click',function(){
+  
+    const perPlayer=input('perPlayer-input')
+    
+    let lists=document.querySelectorAll('li');
+    let listQuantity=lists.length;
+    const playercost=perPlayer*listQuantity;
+
+    const playerExpenses=document.getElementById('playerExpenses-input');
+    playerExpenses.innerText=playercost;
+
+    
+
+})
+
+document.getElementById('calculateTotal-btn').addEventListener('click',function(){
+
+    const perPlayer=input('perPlayer-input');
+    
+
+    let lists=document.querySelectorAll('li');
+    let listQuantity=lists.length;
+    const playercost=perPlayer*listQuantity;
+
+    const manager=input('manager-input');
+    const coach=input('coach-input');
+    
+
+    const total=document.getElementById('total');
+    total.innerText=playercost+manager+coach;
+
+
+})
